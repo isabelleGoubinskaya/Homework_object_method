ae6677366841b3fb04e6d2a97a513652c8b4ac87
Isabelle <isagoubinskaya@gmail.com>
2023-06-10 21:50:07 +0200

    updated exercises

--- src/Author.java
+++ src/Author.java
@@ -1,26 +1,41 @@
+import java.util.Objects;
+
 public class Author {
     private String firstName;
     private String lastName;
-    Author(String firstName, String lastName) {
+
+    public Author(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-    String getFirstName() {
+
+    public String getFirstName() {
         return firstName;
     }
-    String getLastName() {
+
+    public String getLastName() {
         return lastName;
     }
-    public String toString(){
-        return firstName+" "+lastName;
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Author other = (Author) obj;
+        return firstName.equals(other.firstName) && lastName.equals(other.lastName);
+    }
+
+    @Override
     public int hashCode() {
-        return 1;
+        return Objects.hash(firstName, lastName);
     }
-
-
-
-
-
-
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
-
         Author author = new Author("Kilian", "Mbappe");
         Author author1 = new Author("Olivier", "Giroux");
         Book book = new Book("coucou les loulous", "Mbappe", 1970);
         Book book1 = new Book("comprendre l'empire", "Giroux", 1974);
         book1.setYearPublication(1890);
-        System.out.println(book.getNameBook() + " " + author.getFirstName() + " " + author.getLastName() + " " + book.getYearPublication());
-        System.out.println(book1.getNameBook() + " " + author1.getFirstName() + " " + author1.getLastName() + " " + book1.getYearPublication());
 
-        System.out.println("Test equals book " + book.equals(book1));
-        System.out.println("Test equals author " + author.equals(author1));
+        System.out.println(book.toString());
+        System.out.println(book1.toString());
 
+        System.out.println("Test equals book: " + book.equals(book1));
+        System.out.println("Test equals author: " + author.equals(author1));
     }
 }
